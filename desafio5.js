9419d5a47286bacef07420c39aeb0cd1f0e030fb
Nicolas Larrosa <nicolarro20@gmail.com>
2021-10-06 19:36:52 -0300

    Actualizacion desafio 5

--- desafio5.js
+++ desafio5.js
@@ -1,109 +1,82 @@
 
-/* CLASE PRODUCTOS CREADA */
-class Producto{
 
-    constructor(nombre, categoria, precio){
-    this.nombre = nombre;
-    this.categoria = categoria;
-    this.precio = parseFloat(precio);
-    }
-    sumaIva(){
-        this.precio = this.precio * 1.21;
-    }
-    
+
+/* CLASE PRODUCTOS CREADA */
+class Producto {
+constructor(nombre, categoria, precio) {
+this.nombre = nombre;
+this.categoria = categoria;
+this.precio = parseFloat(precio);
+}
+sumaIva() {
+ this.precio = this.precio * 1.21;
+}
 }
 
-const tarta = new Producto("torta", "dulce", 1200);
-const bocados = new Producto ("bocados", "salados", 200);
+/* CREA LAS INSTANCIAS DEL OBJETO PRODUCTO  */
 
+const torta = new Producto("torta", "dulce", 1200);
+const bocados = new Producto("bocados", "salados", 200);
+const tarta = new Producto("tarta", "dulce", 500);
+
+
+/* CREA LISTA PRODUCTOS CON TODOS LOS PRODUCTOS DISPONIBLES */
 
 let productos = [];
 
-/* CLASE TIENDA CREADA */
+productos.push(torta);
+productos.push(tarta);
+productos.push(bocados);
 
-class Tienda{
-    constructor(nombre, productos){
-    this.nombre = nombre;
-    this.productos = [] ;
-}
-agregarproducto(producto){
-    this.productos.push(producto)
-}
-}
-
-const tienda1 = new Tienda("TiendaCatering")
-tienda1.agregarproducto(tarta);
-tienda1.agregarproducto(bocados);
-
-/*------------------------------------------------- SIMULADOR ------------------------------------------------------------------------- */
-
-
-/* -----CALCULAR EL COSTO TOTAL DE LOS PRODUCTOS SELECCIONADOS (PRECIO POR CANTIDAD DEL PRODUCTO), Y MOSTRARLO AL USUARIO--------- */
+/*------------------------------------------------- SIMULADOR ------------------------------------------------------------------------- *//* -----CALCULAR EL COSTO TOTAL DE LOS PRODUCTOS SELECCIONADOS (PRECIO POR CANTIDAD DEL PRODUCTO), Y MOSTRARLO AL USUARIO--------- */
 
 /* SOLICITAR LA CANTIDAD DE PRODUCTOS A ELEGIR */
 
-let cantidad_productos = Number(prompt("Ingrese la cantidad del producto a comprar: "))
+let cantidad_productos = Number(prompt("Ingrese la cantidad del producto a comprar: "));
 
-console.log(cantidad_productos)
+console.log(cantidad_productos);
+
+/* USUARIO SELECCIONA LOS PRODUCTOS A COMPRAR  */
 
 
-
-/* SELECCIONAR EL TIPO DE PRODUCTO A ELEGIR */
-
-/* A SU VEZ GUARDA EL COSTO SEGUN EL PRODUCTO*/
-
-let tipo_productos = prompt("Ingrese el producto a comprar: ").toLowerCase;
+let listadoProducto = [];
 
 function listado_productos(cantidad_productos){
-for (let i= 1; cantidad_productos <5; i++) {
-     tipo_productos = prompt("Ingrese el producto a comprar: ").toLowerCase
-    return tipo_productos;
+for (let i = 1; i <= cantidad_productos; i++){
+let tipo_productos = prompt("Ingrese el producto a comprar: ").toLowerCase();
+
+
+
+const eleccion = productos.filter((item) => item.nombre == tipo_productos);
+
+/* A SU VEZ GUARDA LOS PRODUCTOS ELEGIDOS EN LISTADO PRODUCTOS*/
+listadoProducto.push(...eleccion);
 }
+
+console.log(listadoProducto)
+
+return listadoProducto;
 }
 
+const listado = listado_productos(cantidad_productos);
 
-let costo_producto = ""
+console.log(listado);
 
-function procesa_productos(){
-if (tipo_productos != ""){
-switch(tipo_productos) {
-    case "torta": 
-        costo_producto = 1200;
-        return costo_producto;
-/*         break; */
-    case "bocados": 
-        costo_producto = 200;
-        return costo_producto;
-/*         break; */
-    case "tarta": 
-        costo_producto= 150;
-        return costo_producto;
-/*         break; */
-    default:
-        alert("El producto indicado no esta a la venta");
-        console.log (costo_producto)
-/*         break; */
-    }
+/* CALCULAR EL COSTO TOTAL DE LA COMPRA */
 
-    alert("El costo de" + tipo_productos + "es" + costo_producto);
-    console.log (costo_producto);
-    console.log (tipo_productos);
+let costo_total;
+function monto() {
+let suma = 0;
+listado.forEach((item) => {
+suma += item.precio;
+});
+return suma;
 }
+
+/* LE MUESTRA EL COSTO TOTAL SEGUN LA CANTIDAD Y EL PRODUCTO ELEGIDO */
+function muestraCosto() {
+alert("El costo total de tus productos es " + monto());
+console.log(monto());
 }
-    /* CALCULAR EL COSTO TOTAL DE LA COMPRA */
 
-    let costo_total =""
-
-    function monto () {
-         costo_total = (costo_producto * tipo_productos)
-         console.log(costo_total);
-        return costo_total
-        
-    }
-
-          /* LE MUESTRA EL COSTO TOTAL SEGUN LA CANTIDAD Y EL PRODUCTO ELEGIDO */
-
-    function muestra(){
-        alert("El costo total de tus productos es" + costo_total)
-        console.log(costo_total)
-    }
+muestraCosto();
